d3c3e9c383b322f8445b88261346cfa35ef0fcb4
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2021-09-27 17:17:31 -0600

    added an autorespond to files like the 3ds bots do

--- SysBot.Pokemon.Discord/SysCord.cs
+++ SysBot.Pokemon.Discord/SysCord.cs
@@ -157,13 +157,13 @@ namespace SysBot.Pokemon.Discord
             // Bail out if it's a System Message.
             if (arg is not SocketUserMessage msg)
                 return;
-
+            int pos = 0;
             // We don't want the bot to respond to itself or other bots.
-            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot)
+            if ((msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) && msg.HasStringPrefix(Hub.Config.Discord.CommandPrefix, ref pos))
                 return;
 
             // Create a number to track where the prefix ends and the command begins
-            int pos = 0;
+            
             if (msg.HasStringPrefix(Hub.Config.Discord.CommandPrefix, ref pos))
             {
                 bool handled = await TryHandleCommandAsync(msg, pos).ConfigureAwait(false);
