46405fb39631c14ded25ace9320049620cba52cd
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2021-12-04 10:16:35 -0500

    i think i finally have enough catches, theres probably a better way to go about it but it works lmfao

--- SysBot.Pokemon/LGPETradeBot/LGPETradeBot.cs
+++ SysBot.Pokemon/LGPETradeBot/LGPETradeBot.cs
@@ -261,7 +261,7 @@ namespace SysBot.Pokemon
                     while (await LGIsInTrade(token) && BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != Boxscreen)
                         await Click(A, 1000, token);
                     Log("trade animation complete, catching trade evolutions");
-                    while (BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != Boxscreen && BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != waitingtotradescreen2 && BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != waitingtotradescreen)
+                    while (BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != Boxscreen && BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != waitingtotradescreen2 && BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != waitingtotradescreen && BitConverter.ToUInt16(await SwitchConnection.ReadBytesMainAsync(ScreenOff, 2, token), 0) != menuscreen)
                         await Click(A, 1000, token);
                     Log("Trade should be completed, exiting box");
                    
